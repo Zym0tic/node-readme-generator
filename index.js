979b229d436162b7c113c026d5bd98df557c05de
zym0tic <lukerozon@gmail.com>
2022-08-09 18:46:16 -0400

    vreated prompts in the terminal for Q's

--- index.js
+++ index.js
@@ -2,14 +2,38 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [
-  "What is the title of your application?",
-  "Please describe your application.",
-  "What are the installation instructions?",
-  "Provide usage info.",
-  "What are the contribution guidelines?",
-  "provide test instructions."
-];
+const questions =  inquirer.prompt([
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your application?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Write a description of your application.",
+  },
+  {
+    type:"input",
+    name: "installation",
+    message: "Provide instructions for installing the application."
+  },
+  {
+    type: "input",
+    name: "usageInformation",
+    message: "Provide information of usage for the application.",
+  },
+  {
+    type: "input",
+    name: "contributionGuidelines",
+    message: "what are guidelines for contribution to this application?",
+  },
+  {
+    type: "input",
+    name:"testInstructions",
+    message: "provide instructions for testing teh application"
+  }
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
